6af2758d373451a0e9a128d170e51303fdb004f3
Gurunath <gurunathsolanki@gmail.com>
2020-04-09 19:33:49 +0100

    Pass the course ID to the Course page and output it there

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
-    }
+  state = {
+    courseId: null,
+  };
+  componentDidMount() {
+    console.log('[Course] mounted !!');
+    console.log(this.props);
+
+    this.setState({ courseId: this.props.match.params.id });
+  }
+  render() {
+    return (
+      <div>
+        <h1>_COURSE_TITLE_</h1>
+        <p>You selected the Course with ID: {this.state.courseId}</p>
+      </div>
+    );
+  }
 }
 
 export default Course;
